dead5a8119e8c7f574896419b2dd0f9dfdf54570
Talia Iannuccilli <tsiannuccilli@email.neit.edu>
2021-02-20 15:49:38 -0500

    wk6 work and assignment

---
+++ BlockBusterWebApp/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BlockBusterWebApp.Models;
+
+namespace BlockBusterWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Colors()
+        {
+            string[] colors = { "Red", "Blue", "Yellow" };
+            ViewBag.Colors = colors;
+            return View();
+        }
+
+        public IActionResult Cities()
+        {
+            string[] cities = { "London", "Tokyo", "Dubia", "Rome", "New York" };
+            ViewBag.Cities = cities;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
